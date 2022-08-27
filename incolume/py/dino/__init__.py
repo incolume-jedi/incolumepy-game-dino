f821f8c0d52305172dd78a34f5697c56f05f59a2
Britodfbr <britodfbr@gmail.com>
2022-08-27 07:39:52 -0300

    chore: Unificação de versão para projeto

--- incolume/py/dino/__init__.py
+++ incolume/py/dino/__init__.py
@@ -1,1 +1,11 @@
-__version__ = '0.1.0'
+import toml
+from pathlib import Path
+
+
+pyproject = Path(__file__).parents[3].joinpath('pyproject.toml')
+versionfile = Path(__file__).parents[3] / 'version.txt'
+
+versionfile.write_text(
+    f"{toml.load(pyproject)['tool']['poetry']['version']}\n"
+)
+__version__ = versionfile.read_text().strip()
